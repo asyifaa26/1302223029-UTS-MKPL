8b0bc542e13ec25257061fd460c681f70a1d86dd
asyifaa26 <153969653+asyifaa26@users.noreply.github.com>
2025-04-19 00:50:10 +0700

    Refactoring class Employee dengan Extract method untuk penanganan validasi dan relief

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -15,30 +15,35 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
+	 public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        validateInputs(numberOfMonthWorking);
+
+        int taxRelief = getTaxRelief(isMarried, numberOfChildren);
+        int annualIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+        int taxableIncome = annualIncome - deductible - taxRelief;
+
+        int tax = (int) Math.round(0.05 * taxableIncome);
+        return Math.max(tax, 0);
+    }
+
+    private static void validateInputs(int numberOfMonthWorking) {
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 month working per year");
+        }
+    }
+
+    private static int getTaxRelief(boolean isMarried, int numberOfChildren) {
+        final int BASE_RELIEF = 54000000;
+        final int MARRIED_RELIEF = 4500000;
+        final int CHILD_RELIEF = 1500000;
+
+        int relief = BASE_RELIEF;
+        if (isMarried) {
+            relief += MARRIED_RELIEF;
+        }
+
+        relief += CHILD_RELIEF * Math.min(3, numberOfChildren);
+        return relief;
+    }
 	
 }
