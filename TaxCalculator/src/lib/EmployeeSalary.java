da37d72fc189c3d13b1e32cd3bfc6eda2ad86732
asyifaa26 <153969653+asyifaa26@users.noreply.github.com>
2025-04-19 03:13:23 +0700

    Menghapus temporary field dari Employee
    Menggunakan variabel lokal untuk monthWorkingInYear

--- TaxCalculator/src/lib/EmployeeSalary.java
+++ TaxCalculator/src/lib/EmployeeSalary.java
@@ -8,6 +8,7 @@ public class EmployeeSalary {
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
+    
     public void setMonthlySalary(int grade, boolean isForeigner) {
         if (grade == 1) {
             monthlySalary = 3000000;
